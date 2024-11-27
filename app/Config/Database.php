4119204b23f22ec7087fd77ccf35e1470acad404
alexis-1729 <ibaa59733@gmail.com>
2024-11-26 21:11:18 -0600

    Configuracion adicional para riveadmin

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -29,7 +29,7 @@ class Database extends Config
         'hostname'     => 'localhost',
         'username'     => 'root',
         'password'     => '',
-        'database'     => 'riversb',
+        'database'     => 'ingetec1_riversafe',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
